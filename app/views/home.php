a26f89cbdfb9ee2f4ff38ecbab83c3bb88c5680e
Ankit Chand <t-achand@expediagroup.com>
2021-10-05 22:39:18 +0530

    login/register system

--- app/views/home.php
+++ app/views/home.php
@@ -1,2 +1,11 @@
+<?php if($this->session->flashdata('registered')): ?>
+	<p class="alert alert-dismissable alert-success"><?php echo $this->session->flashdata('registered'); ?></p>
+<?php endif; ?>
+<?php if($this->session->flashdata('login_success')): ?>
+	<p class="alert alert-dismissable alert-success"><?php echo $this->session->flashdata('login_success'); ?></p>
+<?php endif; ?>
+<?php if($this->session->flashdata('login_failed')): ?>
+	<p class="alert alert-dismissable alert-danger"><?php echo $this->session->flashdata('login_failed'); ?></p>
+<?php endif; ?>
 <h1> Welcome to myTodo</h1>
 <p>myTodo is a simple</p>
--- app/views/layouts/main.php
+++ app/views/layouts/main.php
@@ -16,8 +16,13 @@
 				<a class="brand" href="<?php echo base_url(); ?>">myTodo</a>
 				<div class="nav-collapse collapse">
 					<p class="navbar-text pull-right">
-						<a href="<?php echo base_url(); ?>users/register">Register</a>
-						
+						<!--RIGHT TOP CONTENT-->
+			             <?php if($this->session->userdata('logged_in')) : ?>
+			               Welcome,  <?php echo $this->session->userdata('username'); ?>
+			             <?php else : ?>
+			                <a href="<?php echo base_url(); ?>users/register">Register</a>
+			             <?php endif; ?>
+									
 					</p>
 					<ul class="nav">
 						<li><a href="<?php echo base_url(); ?>">Home</a></li>
