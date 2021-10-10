1f0e6d784e8cb7e741612f31e6331fe188cf2abf
Ankit Chand <t-achand@expediagroup.com>
2021-10-10 18:32:05 +0530

    model list crud operations

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -5,6 +5,14 @@
 			// //how to pass data to view
 			// $data['welcome'] = 'Hello!';
 			// $this->load->view('home', $data);
+			if($this->session->userdata('logged_in')){
+            //Get the logged in users id
+	            $user_id = $this->session->userdata('user_id');
+	            //Get all lists from the model
+	            $data['lists'] = $this->List_model->get_all_lists($user_id);
+	           // $data['tasks'] = $this->Task_model->get_users_tasks($user_id);
+	        }
+       
 
 			$data['main_content'] = 'home';
 			$this->load->view('layouts/main', $data);
--- app/views/home.php
+++ app/views/home.php
@@ -7,5 +7,26 @@
 <?php if($this->session->flashdata('login_failed')): ?>
 	<p class="alert alert-dismissable alert-danger"><?php echo $this->session->flashdata('login_failed'); ?></p>
 <?php endif; ?>
+<?php if($this->session->flashdata('noaccess')): ?>
+	<p class="alert alert-dismissable alert-danger"><?php echo $this->session->flashdata('noaccess'); ?></p>
+<?php endif; ?>
 <h1> Welcome to myTodo</h1>
 <p>myTodo is a simple</p>
+
+<h3>My Latest Lists</h3>
+<table class="table table-striped" width="50%" cellspacing="5" cellpadding="5">
+    <tr>
+        <th>List Name</th>
+        <th>Created On</th>
+        <th>View</th>
+    </tr>
+    <?php if(isset($lists)) : ?>
+    <?php foreach($lists as $list) : ?>
+    <tr>
+        <td><?php echo $list->list_name; ?></td>
+        <td><?php echo $list->create_date; ?></td>
+        <td><a href="<?php echo base_url(); ?>lists/show/<?php echo $list->id; ?>">View List</a></td>
+    </tr>
+    <?php endforeach; ?>
+    <?php endif; ?>
+</table>
